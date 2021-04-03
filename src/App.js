f4e8e43e244b381cc0846db3fb344b0bb39de104
FrazShabbir <fraz.shabbir54@gmail.com>
2021-04-03 21:15:57 +0500

    d

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   const name = 'Fraz Shabbir';
   const msg = 'My name is fraz shabbir';
+  const msg2 = 'My name is fraz shabbir';
 
   return (
     <div className="App">
       <h1>hello! {name}</h1>
       <p> {msg}</p>
+      <p> {msg2}</p>
      
     </div>
   );
