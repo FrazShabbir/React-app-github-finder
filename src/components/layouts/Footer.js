c7d1854de028a1622f7acaa72d9a0795539a6cc1
Fraz <fraz.shabbir54@gmail.com>
2021-07-28 21:02:16 +0500

    added footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -71,29 +71,26 @@ export class Footer extends Component {
                     </div>
 
                 </footer> */}
-<footer className="page-footer font-small unique-color-dark pt-4 bg-danger">
+                <footer className="page-footer font-small unique-color-dark pt-4 bg-danger">
 
-  <div className="container">
+                    <div className="container">
 
-    <ul className="list-unstyled list-inline text-center py-2">
-      <li className="list-inline-item">
-        <h5 className="mb-1">Register for free</h5>
-      </li>
-      <li className="list-inline-item">
-        <a href="#!" className="btn btn-outline-white btn-rounded">Sign up!</a>
-      </li>
-    </ul>
+                        <ul className="list-unstyled list-inline text-center py-2">
+                            <li className="list-inline-item">
+                                <h5 className="mb-1">Register for free</h5>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="#!" className="btn btn-outline-white btn-rounded">Sign up!</a>
+                            </li>
+                        </ul>
 
-  </div>
+                    </div>
 
-  <div className="footer-copyright text-center py-3 ">© 2020 Copyright:&nbsp;&nbsp;
-    <a href="https://frazshabbir.com/" className='text-dark'>FrazShabbir.com</a>
-  </div>
+                    <div className="footer-copyright text-center py-3 ">© 2020 Copyright:&nbsp;&nbsp;
+                        <a href="https://frazshabbir.com/" className='text-dark'>FrazShabbir.com</a>
+                    </div>
 
-</footer>
-
-
-
+                </footer>
 
 
             </div>
